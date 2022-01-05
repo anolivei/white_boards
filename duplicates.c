784bf8ebc14b28c13978ed3b6d4c8c2060ff6423
Angélica Oliveira Miranda <59845902+anolivei@users.noreply.github.com>
2022-01-05 11:42:27 -0300

    comentarios

--- duplicates.c
+++ duplicates.c
@@ -1,3 +1,18 @@
+/*
+Exercício feito no whiteboard com os
+recrutadores do mercado livre
+
+Foi soilicitado para verificar números duplicados
+em um array de ints
+
+Nessa implementação feita em C
+provavelmente daria um timeout
+devido ao while dentro do while,
+sendo necessário percorrer o array
+len(array) vezes
+(com percurso menor a cada vez)
+*/
+
 #include "stdio.h"
 #include "stdbool.h"
 #include <stdlib.h>
