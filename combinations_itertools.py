786bb6cd1765dc9ea291c329c58d2dc74022a04a
Angélica Oliveira Miranda <59845902+anolivei@users.noreply.github.com>
2022-01-09 00:38:15 -0300

    flake8

--- combinations_itertools.py
+++ combinations_itertools.py
@@ -1,11 +1,11 @@
-""" 
+"""
 Exercício feito no Hackerrank:
  - Como parâmetros de entrada são dados
  um array de ints "d" e um int "t"
  - Deve-se calcular quantas combinações
  de três números em "d" têm a soma
  menor do que "t"
- 
+
 Nessa implementação é utlizada a
 lib intertools para fazer uma
 combinação de três em três no
