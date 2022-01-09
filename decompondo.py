5a50cd75e62ff4a60e1528a11acbe0c0129259ca
Angélica Oliveira Miranda <59845902+anolivei@users.noreply.github.com>
2022-01-09 00:37:44 -0300

    decompondo

--- decompondo.py
+++ decompondo.py
@@ -9,17 +9,19 @@ Print output to STDOUT
 """
 import itertools
 
+
 def decompondo():
-	max_num = int(input().rstrip("\n"))
-	ret = 0
-	for i in itertools.product(range(0, max_num + 1), repeat=4):
-		if (sum(i)) == 21:
-			ret += 1
-			print(i)
-			#print(str(i[0]) + str(i[1]) + str(i[2]) + str(i[3]))
-	if ret == 0:
-		print('null')
+    max_num = int(input().rstrip("\n"))
+    ret = 0
+    for i in itertools.product(range(0, max_num + 1), repeat=4):
+        if (sum(i)) == 21:
+            ret += 1
+            # print(i)
+            # print(f"{i[0]}{i[1]}{i[2]}{i[3]}")
+            print(str(i[0]) + str(i[1]) + str(i[2]) + str(i[3]))
+    if ret == 0:
+        print('null')
+
 
 if __name__ == '__main__':
-	decompondo()
-
+    decompondo()
